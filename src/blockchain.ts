79964a873999ac0c9ccc337e08f8432df17654e6
Bima Aulia <bimaaulia.15@gmail.com>
2024-05-05 00:10:47 +0700

    refactor: exclude several file from git

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -1,12 +1,17 @@
 import Block from "./block"
-import cryptoHash from "./crypto-hash"
-import type { BlockchainInterface } from "./types"
+import Transaction from "./transaction"
 
-class Blockchain implements BlockchainInterface {
+import { cryptoHashV2 } from "./crypto-hash"
+import { MINT_KEY_PAIR, MINT_PUBLIC_ADDRESS } from "./config"
+
+class Blockchain {
+  chain: Block[]
+  transactions: Array<Transaction>
+
   constructor() {
     this.chain = [Block.genesis()]
+    this.transactions = []
   }
-  chain: Block[]
 
   static isValidChain(chain: Array<Block>) {
     if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis()))
@@ -18,7 +23,7 @@ class Blockchain implements BlockchainInterface {
 
       if (lastHash !== actualLastHash) return false
 
-      const validatedHash = cryptoHash(
+      const validatedHash = cryptoHashV2(
         timestamp,
         lastHash,
         data,
@@ -35,10 +40,14 @@ class Blockchain implements BlockchainInterface {
     return true
   }
 
-  addBlock({ data }: { data: any }) {
+  getLastBlock() {
+    return this.chain[this.chain.length - 1]
+  }
+
+  addBlock({ data }: { data: Array<any> }) {
     const newBlock = Block.mineBlock({
       data,
-      lastBlock: this.chain[this.chain.length - 1],
+      lastBlock: this.getLastBlock(),
     })
 
     this.chain.push(newBlock)
@@ -55,6 +64,24 @@ class Blockchain implements BlockchainInterface {
     }
 
     this.chain = newChain
+  }
+
+  mineTransaction(rewardAddress: string) {
+    const rewardTransaction = new Transaction({
+      from: MINT_PUBLIC_ADDRESS,
+      to: rewardAddress,
+      data: [],
+    })
+    rewardTransaction.sign(MINT_KEY_PAIR)
+
+    this.addBlock({
+      data: [rewardTransaction, ...this.transactions],
+    })
+    this.transactions = []
+  }
+
+  addTransaction(transaction: Transaction) {
+    if (transaction.isValid()) this.transactions.push(transaction)
   }
 }
 
